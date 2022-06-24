0d6bc7db246f2e7338cb8776aaecc8413f34fd7d
lilys tacos <lilyannek@gmail.com>
2022-06-23 18:55:51 -0700

    addition of if statements

--- script.js
+++ script.js
@@ -7,9 +7,36 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-alert("What would you like to include in your password?");
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+document.querySelector("#generate").addEventListener("click", generatePassword);
+
+// var passLength = prompt("How long would you like your password to be? Enter a number between 8 and 128.");
+// var charType = prompt("Do you want to include different character types in your password?");
+
+
+  
+function generatePassword = prompt("How long would you like your password to be?");
+
+var passwordLength = prompt("How long would you like your password to be?")
+ if (passwordLength < 8 || passwordLength > 129) {
+    alert("Password must be between 8 and 128 characters long.")
+ }
+  
+ if (confirm("Would you like your password to include upper case letters?"));
+   
+ if (confirm("Would you like your password to include lower case letters?"));
+
+ if (confirm("Would you like your password to include numbers?"));
+
+ if (confirm("Would you like your password to include special characters?"));
+
+
+  // if (passwordLength <= 8) {
+  //  alert("Password must be at least 8 characters long.");
+  //  } else if (passwordLength => 128) {
+  //    alert("Password must be 128 characters or less.")
+  //  }
+
