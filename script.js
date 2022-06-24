a5193f0fb070ad4908b4999b0f83f203e3466afe
lilys tacos <lilyannek@gmail.com>
2022-06-23 23:55:08 -0700

    creation of functions for asking user to confirm inclusion of character types for password

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
 // Arrays for all possible character types
 const abcLower = ["bcdefghijklmnopqrstuvwxyz"];
 const abcUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 const specialChar = ["!@#$%^&*~"];
 const numbers = ["0123456789"];
 
-// var askAbcLower = confirm("Would you like your password to include lower case letters?");
-//var askAbcUpper = confirm("Would you like your password to include upper case letters?");
-//var askspecialChar = confirm("Would you like your password to include special characters?");
-//var askNumbers = confirm("Would you like your password to include numbers?");
-
-// Function to ask user about password length
-function promptMe() {
-  var passwordLength = prompt("How long would you like your password to be? Between 8-128 characters.");
+passwordLength = prompt("How long would you like your password to be? Between 8-128 characters.");
   if (passwordLength < 8 || passwordLength > 129) {
     alert("Password must be between 8 and 128 characters long.");
-
-  } else if (askAbcLower) = confirm("Do you want to include lowercase letters in your password?");
 }
-  // } else if askAbcUpper =  confirm("Do you want to include uppercase letters in your password?"); {
 
-  // } else if askSpecialChar = confirm("Do you want to include special characters in your password?")
+passwordLowercase = confirm("Do you want to inlude lowercase letters in your password?");
+if (passwordLowercase) {
+  alert("Your password will include lowercase letters.");
+} else alert("Your password will not include lowercase letters.");
 
-// The confirm() method returns true if the user clicked "OK", otherwise false.
+passwordUppercase = confirm("Do you want to include uppercase letters in your password?");
+if (passwordUppercase) {
+  alert("Your password will include uppercase letters.")
+} else alert("You password will not include uppercase letters.");
+
+passwordSpecialChar = confirm("Do you want to include special characters in your password?");
+if (passwordSpecialChar) {
+  alert("Your password will include special characters.")
+} else alert("Your password will not include special characters.");
+
+passwordNumbers = confirm("Do you want to include numbers in your password?")
+if (passwordNumbers) {
+  alert("Your password will include numbers.")
+} else alert("Your password will not include numbers.");
+
+if (passwordLowercase === false && passwordUppercase === false && passwordSpecialChar === false && passwordNumbers === false) {
+  return "Please include at least one character type.";
+}
 
 // Function combines all user responses and produces desired password
 function generatePassword() {
